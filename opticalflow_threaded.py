c235290787c2bbed8d11ad4b7fbe38f4ebbb2ff6
NetSys <sagarjoglekar@nmsdps000222.nms.kcl.ac.uk>
2015-11-20 15:52:41 +0000

    some changesx

--- opticalflow_threaded.py
+++ opticalflow_threaded.py
@@ -1,60 +1,101 @@
+#!/usr/bin/env python
+
+'''
+Multithreaded video processing sample.
+Usage:
+   video_threaded.py {<video device number>|<video file name>}
+
+   Shows how python threading capabilities can be used
+   to organize parallel captured frame processing pipeline
+   for smoother playback.
+
+Keyboard shortcuts:
+
+   ESC - exit
+   space - switch between multi and single threaded processing
+'''
+
+
 import numpy as np
 import cv2
+
+from multiprocessing.pool import ThreadPool
+from collections import deque
+
+from common import clock, draw_str, StatValue
 import video
 
-try: fn = sys.argv[1]
-except: fn = 0
-cap = video.create_capture(fn)
 
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
+class DummyTask:
+    def __init__(self, data):
+        self.data = data
+    def ready(self):
+        return True
+    def get(self):
+        return self.data
 
-# Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 2,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+if __name__ == '__main__':
+    import sys
 
-# Create some random colors
-color = np.random.randint(0,255,(100,3))
+    print __doc__
 
-# Take first frame and find corners in it
-ret, old_frame = cap.read()
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    try: fn = sys.argv[1]
+    except: fn = 0
+    cap = video.create_capture(fn)
+    
+    # params for ShiTomasi corner detection
+    feature_params = dict( maxCorners = 100,qualityLevel = 0.3,minDistance = 7,blockSize = 7 )
+    # Parameters for lucas kanade optical flo
+    lk_params = dict( winSize  = (15,15), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+    # Create some random colors
+    color = np.random.randint(0,255,(100,3))
+    # Take first frame and find corners in it
+    ret, old_frame = cap.read()
+    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(old_frame)
 
-# Create a mask image for drawing purposes
-mask = np.zeros_like(old_frame)
 
-while(1):
-    ret,frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    def process_frame(frame, t0):
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# calculate optical flow
+	p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+	# Select good points
+	good_new = p1[st==1]
+	good_old = p0[st==1]
 
-    # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-    # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
+    threadn = cv2.getNumberOfCPUs()
+    pool = ThreadPool(processes = threadn)
+    pending = deque()
 
-    # draw the tracks
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    img = cv2.add(frame,mask)
+    threaded_mode = True
 
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
-
+    latency = StatValue()
+    frame_interval = StatValue()
+    last_frame_time = clock()
+    while True:
+        while len(pending) > 0 and pending[0].ready():
+            res, t0 = pending.popleft().get()
+            latency.update(clock() - t0)
+            draw_str(res, (20, 20), "threaded      :  " + str(threaded_mode))
+            draw_str(res, (20, 40), "latency        :  %.1f ms" % (latency.value*1000))
+            draw_str(res, (20, 60), "frame interval :  %.1f ms" % (frame_interval.value*1000))
+            cv2.imshow('threaded video', res)
+        if len(pending) < threadn:
+            ret, frame = cap.read()
+            t = clock()
+            frame_interval.update(t - last_frame_time)
+            last_frame_time = t
+            if threaded_mode:
+                task = pool.apply_async(process_frame, (frame.copy(), t))
+            else:
+                task = DummyTask(process_frame(frame, t))
+            pending.append(task)
+        ch = 0xFF & cv2.waitKey(1)
+        if ch == ord(' '):
+            threaded_mode = not threaded_mode
+        if ch == 27:
+            break
 cv2.destroyAllWindows()
-cap.release()
